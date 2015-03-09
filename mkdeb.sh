024b7e6fe1d42456b0cdf070cf10cfb20d4a62d5
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2015-03-09 15:48:41 +0000

    0.9.22 testing
    git-svn-id: svn://svn.code.sf.net/p/firejail/code-0/trunk@449 a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc

--- mkdeb.sh
+++ mkdeb.sh
@@ -68,6 +68,7 @@ find ./debian -type d | xargs chmod 755
 dpkg-deb --build debian
 lintian debian.deb
 mv debian.deb firejail_$2_1_amd64.deb
+echo "if building a 32bit package, rename the deb file manually"
 rm -fr debian
 rm -fr $CODE_DIR
 
