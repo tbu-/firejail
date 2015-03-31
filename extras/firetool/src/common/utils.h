cdff8e1fc8b3c593003304dc655d8ed22fe8f9f1
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2015-03-31 15:08:57 +0000

    rename firetool project
    git-svn-id: svn://svn.code.sf.net/p/firejail/code-0/trunk@492 a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc

--- extras/firetool/src/common/utils.h
+++
@@ -1,6 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-char *run_program(const char *prog);
-bool which(const char *prog);
-#endif
--- extras/firetool/src/firetool/graph.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAPH_h
-#define GRAPH_h
-#include <QString>
-
-class DbPid;
-QString graph(int id, DbPid *dbpid, int cycle);
-
-
-#endif
