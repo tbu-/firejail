4ac52eaf8fc585c9cebfdb65c194e25940585ed0
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2015-04-24 13:53:57 +0000

    add join support for user namespaces
    git-svn-id: svn://svn.code.sf.net/p/firejail/code-0/trunk@534 a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc

--- src/firejail/join.c
+++ src/firejail/join.c
@@ -144,6 +144,40 @@ static void extract_caps_seccomp(pid_t pid) {
 	free(file);
 }
 
+void extract_user_namespace(pid_t pid) {
+	// test user namespaces available in the kernel
+	struct stat s1;
+	struct stat s2;
+	struct stat s3;
+	if (stat("/proc/self/ns/user", &s1) == 0 &&
+	    stat("/proc/self/uid_map", &s2) == 0 &&
+	    stat("/proc/self/gid_map", &s3) == 0);
+	else
+		return;
+			
+	// read uid map
+	char *uidmap;
+	if (asprintf(&uidmap, "/proc/%u/uid_map", pid) == -1)
+		errExit("asprintf");
+	FILE *fp = fopen(uidmap, "r");
+	if (!fp) {
+		free(uidmap);
+		return;
+	}
+
+	// check uid map
+	int u1;
+	int u2;
+	if (fscanf(fp, "%d %d", &u1, &u2) == 2) {
+		if (arg_debug)
+			printf("User namespace detected: %s, %d, %d\n", uidmap, u1, u2);
+		if (u1 != 0 || u2 != 0)
+			arg_noroot = 1;
+	}
+	fclose(fp);
+	free(uidmap);
+}
+
 void join_name(const char *name, const char *homedir, int argc, char **argv, int index) {
 	if (!name || strlen(name) == 0) {
 		fprintf(stderr, "Error: invalid sandbox name\n");
@@ -199,6 +233,7 @@ void join(pid_t pid, const char *homedir, int argc, char **argv, int index) {
 		extract_cpu(pid);
 		extract_cgroup(pid);
 		extract_nogroups(pid);
+		extract_user_namespace(pid);
 	}
 	
 	// set cgroup
@@ -261,6 +296,13 @@ void join(pid_t pid, const char *homedir, int argc, char **argv, int index) {
 			errExit("setenv");
 		// drop privileges
 		drop_privs(arg_nogroups);
+
+		if (arg_noroot) {
+			if (arg_debug)
+				printf("Joining user namespace\n");
+			if (join_namespace(1, "user"))
+				exit(1);
+		}
 
 		// set prompt color to green
 		//export PS1='\[\e[1;32m\][\u@\h \W]\$\[\e[0m\] '
